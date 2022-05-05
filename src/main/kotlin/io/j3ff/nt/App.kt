bedbaa38d9bfc241132095e1db2cc16d1bfeceb5
Jeff Hutchison <jeff@jeffhutchison.com>
2022-05-05 19:18:04 -0400

    Add shutdown hook, bump wpi version

--- src/main/kotlin/io/j3ff/nt/App.kt
+++ src/main/kotlin/io/j3ff/nt/App.kt
@@ -5,18 +5,20 @@ import edu.wpi.first.networktables.NetworkTableInstance
 
 fun main() {
     val networkIdentity = System.getenv("NT_IDENTITY") ?: "NetworkTables Server"
-    val persistFile = "${ System.getenv("STATE_DIRECTORY") ?: "/tmp" }/networktables.ini"
+    val persistFile = "${System.getenv("STATE_DIRECTORY") ?: "/tmp"}/networktables.ini"
     val port = System.getenv("NT_PORT")?.toIntOrNull() ?: 1735
 
-    NetworkTableInstance.getDefault().apply {
+    val nt = NetworkTableInstance.getDefault().apply {
         addLogger({ println(it.message) }, 11, 100)
         startServer()
     }
+
+    Runtime.getRuntime().addShutdownHook(Thread { nt.stopServer() })
 
     println("$networkIdentity listening on port $port")
     println("$networkIdentity saving state to $persistFile")
 
     do {
-        Thread.sleep(2500L)
+        Thread.sleep(500L)
     } while (true)
 }
